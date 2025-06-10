d8d39c38a6624560c465365c2195d2a6fa838e6d
RaihaneBenjilali <raihane.benjilali@student.odisee.be>
2025-06-10 09:41:49 +0200

    setup

---
+++ src/test/java/be/ehb/aquafin_case_2/AquafinCase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.aquafin_case_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquafinCase2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
